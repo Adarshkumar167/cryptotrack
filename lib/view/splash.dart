a0ff5927cb8fd5e081b9e3ff4a785e9c321b1b4d
Adarsh <adarshkumar167@gmail.com>
2023-04-22 16:05:08 +0530

    Crypto_tracker_app

---
+++ lib/view/splash.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    double myHeight = MediaQuery.of(context).size.height;
+    double myWidth = MediaQuery.of(context).size.width;
+
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        height: myHeight,
+        width: myWidth,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.asset('assets/images/1.gif'),
+            const Column(
+              children: [
+                Text(
+                  'The Future',
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Learn more about cryptocurrency, look to ',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.grey,
+                  ),
+                ),
+                Text(
+                  'the future in IO Crypto',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: myWidth * 0.15),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xffFBC700),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: myWidth * 0.05, vertical: myHeight * 0.01),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'CREATE PORTFOLIO',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      RotationTransition(
+                        turns: AlwaysStoppedAnimation(310 / 360),
+                        child: Icon(Icons.arrow_forward_rounded),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
